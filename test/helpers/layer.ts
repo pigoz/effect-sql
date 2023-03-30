399464c09fbe4312cec0b91fdfd800d428077f2f
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2023-03-30 12:31:56 +0200

    feat(layer): use config

--- test/helpers/layer.ts
+++ test/helpers/layer.ts
@@ -5,35 +5,55 @@ import { PostgreSqlContainer } from "testcontainers";
 import { Pool } from "pg";
 import * as path from "path";
 import { PgConnection, PgConnectionPool, migrate } from "effect-drizzle/pg";
+import * as Config from "@effect/io/Config";
+import * as ConfigProvider from "@effect/io/Config/Provider";
+import * as ConfigSecret from "@effect/io/Config/Secret";
+import * as ConfigError from "@effect/io/Config/Error";
 
-const DrizzlePgConnectionTest = Layer.effect(
-  PgConnection,
-  pipe(
-    Effect.promise(async () => {
-      const container = await new PostgreSqlContainer()
-        .withUsername("postgres")
-        .withPassword("postgres")
-        .withDatabase("effect_drizzle_test")
-        .withReuse()
-        .start();
+export const config = Config.all({
+  DATABASE_URL: Config.secret(),
+});
 
-      const connectionString =
-        container.getConnectionUri() + "?sslmode=disable";
-
-      const pool = new Pool({ connectionString });
-
-      return new PgConnectionPool(pool);
+export const fromEnv = pipe(
+  ConfigProvider.fromEnv().load(
+    Config.all({
+      DATABASE_URL: Config.secret(),
     })
-  )
+  ),
+  Effect.map((_) => _.DATABASE_URL),
+  Effect.map(ConfigSecret.value)
 );
 
-const DrizzlePgMigration = Layer.effectDiscard(
+export const fromTestContainer = pipe(
+  Effect.promise(async () => {
+    const container = await new PostgreSqlContainer()
+      .withUsername("postgres")
+      .withPassword("postgres")
+      .withDatabase("effect_drizzle_test")
+      .withReuse()
+      .start();
+
+    return container.getConnectionUri() + "?sslmode=disable";
+  })
+);
+
+const PgConnectionTest = (
+  config: Effect.Effect<never, ConfigError.ConfigError, string>
+) =>
+  Layer.effect(
+    PgConnection,
+    pipe(
+      config,
+      Effect.map((connectionString) => ({ connectionString })),
+      Effect.map((config) => new PgConnectionPool(new Pool(config)))
+    )
+  );
+
+const PgMigration = Layer.effectDiscard(
   migrate(path.resolve(__dirname, "../migrations/pg"))
 );
 
 export type TestLayer = PgConnection;
 
-export const testLayer: Layer.Layer<never, never, TestLayer> = pipe(
-  DrizzlePgConnectionTest,
-  Layer.provideMerge(DrizzlePgMigration)
-);
+export const testLayer: Layer.Layer<never, ConfigError.ConfigError, TestLayer> =
+  Layer.provideMerge(PgConnectionTest(fromTestContainer), PgMigration);
